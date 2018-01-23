4e4f6c3769a9cd1b14981d95bbbae5fdcc5b6e9d
Marc Molta <MMolta@dvrpc.org>
2018-01-23 15:41:17 -0500

    new images

--- js/index.js
+++ js/index.js
@@ -2,10 +2,16 @@ const tcdiScript = 'http://staging.dvrpc.org/asp/google/drive/tcdi/?year=2012';
 
 // Accordion functionality 
 var accordion = document.querySelectorAll('.accordion')
+var accordionButtons = $('.accordion-controls li button');
 
 for(var i = 0; i < accordion.length; i++){
     accordion[i].onclick = function(){
+        // show/hide the accordions on click
         this.classList.toggle('active')
+        // toggle aria-controls attributes to true/false depending on toggle (or just do opposite since it's onclick?)
+        let expandedOrNot = this.getAttribute('aria-expanded')
+        console.log('expanded or not ', expandedOrNot)
+
         var panel = this.nextElementSibling
         if(panel.style.maxHeight){
             panel.style.maxHeight = null
@@ -15,6 +21,5 @@ for(var i = 0; i < accordion.length; i++){
     }
 }
 
-// Accordion toggling
 
 // REMINDER: when dynamicallly populating tables w/API call, set 'municipality' field to null if it has already been encountered
