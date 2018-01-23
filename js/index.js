527b6e43149aa487e86720251df3c81140a5cb83
Marc Molta <MMolta@dvrpc.org>
2018-01-23 16:48:02 -0500

    aria functionality set + new images

--- js/index.js
+++ js/index.js
@@ -2,17 +2,24 @@ const tcdiScript = 'http://staging.dvrpc.org/asp/google/drive/tcdi/?year=2012';
 
 // Accordion functionality 
 var accordion = document.querySelectorAll('.accordion')
-var accordionButtons = $('.accordion-controls li button');
 
 for(var i = 0; i < accordion.length; i++){
     accordion[i].onclick = function(){
         // show/hide the accordions on click
         this.classList.toggle('active')
-        // toggle aria-controls attributes to true/false depending on toggle (or just do opposite since it's onclick?)
-        let expandedOrNot = this.getAttribute('aria-expanded')
-        console.log('expanded or not ', expandedOrNot)
 
-        var panel = this.nextElementSibling
+        // toggle the aria-expanded attribute of the accordion button
+        let ariaExpandedBool = this.getAttribute('aria-expanded')
+        ariaExpandedBool === 'false' ? ariaExpandedBool = 'true' : ariaExpandedBool = 'false'
+        this.setAttribute('aria-expanded', ariaExpandedBool)
+
+        // toggle the aria-hidden attribute of the accordion panel
+        let panel = this.nextElementSibling
+        let ariaHiddenBool = panel.getAttribute('aria-hidden')
+        ariaHiddenBool === 'false' ? ariaHiddenBool = 'true' : ariaHiddenBool = 'false'
+        panel.setAttribute('aria-hidden', ariaHiddenBool)
+
+        // show/hide the panel on click
         if(panel.style.maxHeight){
             panel.style.maxHeight = null
         }else{
